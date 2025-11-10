7e120dbec223c7165afbfdaeb8ea80ac889465a2
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2025-11-10 17:04:14 +0100

    refactor: Responsiveness and layout adjustments

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,34 +5,37 @@ import CustomIcon from "../components/CustomIcon";
 
 function AboutMe() {
   return (
-    <div className="grid-container">
-      <div className="title">
-        <h1>Hadassa Medeiros</h1>
+    <>
+      <div className="bg-layer"> </div>
+
+      <div className="grid-container">
+        <div className="title">
+          <h1>Hadassa Medeiros</h1>
+        </div>
+        <div className="icons">
+          <CustomIcon
+            href="linkedin.com/in/hadassamedeiros"
+            iconImg={lnkdnIcon}
+          />
+          <CustomIcon href="github.com/hadassa-medeiros" iconImg={githubIcon} />
+        </div>
+        <ul className="info">
+          <li>Full-stack developer (front-end focus)</li>
+          <li>Barcelona, Spain</li>
+        </ul>
+        <div className="description">
+          <p>
+            I like things to flow and be uncomplicated. Full stack developer,
+            front-end focus, UX awareness, academic background in Architecture
+            and Urbanism. I specialize in the MERN stack (MongoDB, Express,
+            React, Node.js).
+          </p>
+        </div>
+        <div className="projects">
+          <p className="note">[ projects section in progress ]</p>
+        </div>
       </div>
-      <div className="icons">
-        <CustomIcon
-          href="linkedin.com/in/hadassamedeiros"
-          iconImg={lnkdnIcon}
-        />
-        <CustomIcon href="github.com/hadassa-medeiros" iconImg={githubIcon} />
-      </div>
-      <ul className="info">
-        <li>Full-stack developer (front-end focus)</li>
-        <li>Background in Architecture and Urbanism</li>
-        <li>Barcelona, Spain</li>
-      </ul>
-      <div className="description">
-        <p>
-          I like things to flow and be uncomplicated. Full stack developer,
-          front-end focus, UX awareness, academic background in architecture and
-          urbanism. I specialize in the MERN stack (MongoDB, Express, React,
-          Node.js).
-        </p>
-      </div>
-      <div className="projects">
-        <p className="note">[ projects section in progress ]</p>
-      </div>
-    </div>
+    </>
   );
 }
 
